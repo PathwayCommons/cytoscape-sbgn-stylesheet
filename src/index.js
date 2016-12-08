5ddd4e155e7b3f3275cee631213c8c856f305eb4
Dylan Fong <d2fong@uwaterloo.ca>
2016-12-08 12:22:06 -0500

    class based es6 implementation - uses es6 features - renames renderer to sbgnviz - uses defaultsDeep for options handling

--- src/index.js
+++ src/index.js
@@ -1,85 +1,25 @@
-var sbgnCytoscape = require('cytoscape-for-sbgnviz');
-var augmentCytoscape = require('./augmentCytoscape');
-var graphStyleSheet = require('./style/graphStyleSheet');
+let Cytoscape = require('cytoscape-for-sbgnviz');
+let defaultsDeep = require('lodash.defaultsdeep');
 
-var SbgnRenderer = function (opts) {
+let augment = require('./augmentCytoscape');
+let graphStyleSheet = require('./style/graphStyleSheet');
 
-  if (!(this instanceof SbgnRenderer)) {
-    return new SbgnRenderer();
-  }
+augment( Cytoscape ); // TODO should be removed eventually (just use vanilla cy)
 
-  // we must augment cytoscape to allow us to render sbgn specific
-  // graphics
-  augmentCytoscape(sbgnCytoscape);
-
-  this.opts = opts;
-
-  this.cy = sbgnCytoscape({
-    container: opts.container,
-    style: graphStyleSheet(sbgnCytoscape),
-    boxSelectionEnabled: opts.boxSelectionEnabled || true,
-    showOverlay: opts.showOverlay || false,
-    minZoom: opts.minZoom || false,
-    maxZoom: opts.maxZoom || false,
-    motionBlur: opts.motionBlur || false,
-    wheelSensitivity: opts.wheelSensitivity || 0.1,
-    ready: function () {
-
-    }
-  });
+let defaults = {
+  style: graphStyleSheet(Cytoscape),
+  boxSelectionEnabled: true,
+  showOverlay: false,
+  minZoom: false,
+  maxZoom: false,
+  motionBlur: false,
+  wheelSensitivity: 0.1
 };
 
-// Terrible,
-// this should call the cytoscape add/remove functions
-// perform a layout
-// calculate the paddings
-SbgnRenderer.prototype.renderGraph = function (cytoscapeGraphJson) {
-  var cy = this.cy;
-
-  cy.startBatch();
-  cy.remove('*');
-  cy.add(cytoscapeGraphJson);
-
-  var nodePositions = {};
-  for (var i = 0; i < cytoscapeGraphJson.nodes.length; i++) {
-    var xPos = cytoscapeGraphJson.nodes[i].data.bbox.x;
-    var yPos = cytoscapeGraphJson.nodes[i].data.bbox.y;
-    nodePositions[cytoscapeGraphJson.nodes[i].data.id] = {'x': xPos, 'y': yPos};
+class SBGNViz extends Cytoscape {
+  constructor( options ){
+    super( defaultsDeep( {}, defaults, options ) );
   }
+}
 
-  cy.layout({
-    name: 'preset',
-    positions: nodePositions,
-    fit: true,
-    padding: 50
-  });
-
-  var nodes = cy.nodes();
-  var totalPadding = 0;
-  var numSimpleNodes = 0;
-  for (var j = 0; j < nodes.length; j++) {
-    var currNode = nodes[j];
-    if (currNode.children() == null || currNode.children().length == 0) {
-      totalPadding += Number(currNode.width());
-      totalPadding += Number(currNode.height());
-      numSimpleNodes++;
-    }
-  }
-
-  var compoundPadding = this.opts.compoundPadding || 10;
-  var padding = (compoundPadding / 100) * Math.floor(totalPadding / (2 * numSimpleNodes));
-  if (padding < 5) {
-    padding = 5;
-  }
-
-  var compounds = nodes.filter('$node > node');
-  compounds.css('padding-left', padding);
-  compounds.css('padding-right', padding);
-  compounds.css('padding-top', padding);
-  compounds.css('padding-bottom', padding);
-
-  cy.endBatch();
-  cy.style().update();
-};
-
-module.exports = SbgnRenderer;
+export default SBGNViz;
