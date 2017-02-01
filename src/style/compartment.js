6bb92463c5a456e34acaa35042d8155417e9f468
Dylan Fong <d2fong@uwaterloo.ca>
2017-02-01 17:28:47 -0500

    partial compartment implementation - makes some improvements to the way height/width is calculated

--- src/style/compartment.js
+++ src/style/compartment.js
@@ -15,11 +15,11 @@ const quad4 = '-0.33, 1, -0.95, 0.95, -1, 0.9';
 const points = () => {
   return `${quad1}, ${quad2}, ${quad3}, ${quad4}`;
 };
-const svg = (str) => {
+const svg = (str, width = 100, height = 100) => {
   let parser = new DOMParser();
   let svgText = 
   `
-    <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='500' height='500'>
+    <svg xmlns='http://www.w3.org/2000/svg' version='1.1' width='${width}' height='${height}'>
     ${str}
     </svg>
   `;
@@ -31,12 +31,12 @@ const svg2b64Str = (svg) => {
   return `data:image/svg+xml;base64,${b64Data}`;
 };
 
-const svgUri = (node) => {
-  const sourceAndSink = 
+const svgUri = (node, borderWidth = 3.75) => {
+  const compartment =
   `
-    <path d="M200, 200 V200, 300"/>
+    <rect width="${node.width()}" height="${node.height()}" fill="none" stroke="black" stroke-width="${borderWidth}" />
   `;
-  return svg2b64Str(svg(sourceAndSink));
+  return svg2b64Str(svg(compartment, node.width(), node.height()));
 };
 
 module.exports = {
