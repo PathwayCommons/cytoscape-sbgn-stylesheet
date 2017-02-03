944600819addfd7100f2ff30d282dcfe689d395d
d2fong <d2fong@uwaterloo.ca>
2017-02-03 13:54:43 -0500

    slight path improvements to compartments

--- src/style/compartment.js
+++ src/style/compartment.js
@@ -22,18 +22,19 @@ const svgUri = (node, borderWidth = 3.75) => {
 
   let nh = node.height();
   let nw = node.width();
+
   const compartment =
   `
   <g style="stroke: #6A6A6A; fill: none; stroke-width: ${borderWidth};">
-    <path d="M ${0*nw} ${.06*nh} L ${0*nw} ${.95*nh} Q ${.06*nw} ${nh} ${0.26*nw} ${nh}"/>
+    <path d="M ${0*nw} ${.03*nh} L ${0*nw} ${.97*nh} Q ${0.06*nw} ${nh} ${0.25*nw} ${nh}"/>
     
-    <path d="M ${0.26*nw} ${nh} L ${0.73*nw} ${nh} Q ${0.9*nw} ${nh} ${nw} ${.95*nh}"/>
+    <path d="M ${0.25*nw} ${nh} L ${0.75*nw} ${nh} Q ${0.93*nw} ${nh} ${nw} ${.97*nh}"/>
 
-    <path d="M ${nw} ${.95*nh} L ${nw} ${0.06*nh} Q ${0.9*nw} ${0*nh} ${0.77*nw} ${0*nh}"/>
+    <path d="M ${nw} ${.97*nh} L ${nw} ${0.03*nh} Q ${0.97*nw} ${0*nh} ${0.75*nw} ${0*nh}"/>
 
-    <path d="M ${0.77*nw} ${0*nh} L ${0.26*nw} ${0*nh} Q ${0.06*nw} ${0*nh} ${0*nw} ${0.06*nh}"/>
+    <path d="M ${0.75*nw} ${0*nh} L ${0.25*nw} ${0*nh} Q ${0.06*nw} ${0*nh} ${0*nw} ${0.03*nh}"/>
   </g>
-`;
+  `;
 
   return svgb64Str(compartment, nw, nh, 0, 0, nw, nh);
 };
