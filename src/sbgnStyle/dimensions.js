04375b155326ad2b4f1e8ca7c6454345489a558e
Dylan Fong <d2fong@uwaterloo.ca>
2017-02-27 11:36:28 -0500

    fixed size dimensions for nodes - some nodes have inconsistent sizes in the sbgnml, these specific nodes   classes will have defined default dimensions

---
+++ src/sbgnStyle/dimensions.js
@@ -0,0 +1,8 @@
+
+const sbgnDimensions = new Map()
+.set('simple chemical', {w: 60, h: 60})
+.set('simple chemical multimer', {w: 60, h: 60})
+.set('complex', {w: 100, h: 50})
+.set('complex multimer', {w: 275, h: 500});
+
+module.exports = sbgnDimensions;
