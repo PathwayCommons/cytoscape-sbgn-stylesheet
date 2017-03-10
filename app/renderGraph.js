55ec600de7f9f7f78336f4fa619677a102951696
Dylan Fong <d2fong@uwaterloo.ca>
2017-03-10 13:35:48 -0500

    performance improvements

--- app/renderGraph.js
+++ app/renderGraph.js
@@ -9,22 +9,24 @@ const renderGraph = (cy, fileText) => {
   const graphJSON = convertSbgnml(fileText);
   const trimmedGraph = graphJSON;
 
-  cy.remove('*');
-  cy.add(trimmedGraph);
+  cy.batch(function(){
+    cy.remove('*');
+    cy.add(trimmedGraph);
 
-  var nodePositions = {};
-  for (var i = 0; i < trimmedGraph.nodes.length; i++) {
-    var xPos = trimmedGraph.nodes[i].data.bbox.x;
-    var yPos = trimmedGraph.nodes[i].data.bbox.y;
-    nodePositions[trimmedGraph.nodes[i].data.id] = {'x': xPos, 'y': yPos};
-  }
-  cy.layout({
-    name: 'preset',
-    positions: nodePositions,
-    fit: true,
-    padding: 100
-  })
-  .run();
+    var nodePositions = {};
+    for (var i = 0; i < trimmedGraph.nodes.length; i++) {
+      var xPos = trimmedGraph.nodes[i].data.bbox.x;
+      var yPos = trimmedGraph.nodes[i].data.bbox.y;
+      nodePositions[trimmedGraph.nodes[i].data.id] = {'x': xPos, 'y': yPos};
+    }
+
+    cy.layout({
+      name: 'preset',
+      positions: nodePositions,
+      fit: true,
+      padding: 100
+    }).run();
+  });
 
   initGraphManager(cy);
 };
