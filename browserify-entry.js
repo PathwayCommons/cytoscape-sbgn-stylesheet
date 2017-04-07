f55e754dc6df0fa8fc1e25cad8dd3297ea99404d
d2fong <d2fong@uwaterloo.ca>
2017-04-07 19:24:15 -0400

    update

--- browserify-entry.js
+++ browserify-entry.js
@@ -28,12 +28,51 @@ $(document).ready(function () {
 
   window.SBGNRenderer = SBGNRenderer;
   window.r = window.cy = renderer;
-  const sbgnmlText = file.loadFileText('samples/pc_signallingByBMP.sbgn.xml');
-  renderGraph(renderer, sbgnmlText);
 
   renderer.on('tap', 'node', function (evt) {
     console.log(evt.target);
   });
+  const opts = {
+    fisheye: true,
+    animate: true,
+    undoable: false,
+    cueEnabled: false
+  };
+  renderer.expandCollapse(opts);
+
+  renderer.on('expandcollapse.afterexpand', function (evt) {
+    const node = evt.target;
+    renderer.zoomingEnabled(false);
+    node.children().layout({
+      name:'grid',
+      fit: 'false',
+      avoidOverlap: true,
+      condense: true,
+      animate: true,
+      rows: node.children().size() / 2,
+      cols: node.children().size() / 2,
+      boundingBox: node.boundingBox()
+    }).run();
+    renderer.zoomingEnabled(true);
+  });
+
+  const complexes = renderer.nodes('[class="complex"], [class="complex multimer"]');
+  const api = renderer.expandCollapse('get');
+  api.collapse(complexes);
+
+  renderer.on('tap', 'node[class="complex"], node[class="complex multimer"]', (evt) => {
+    evt.preventDefault();
+    const node = evt.target;
+    if (api.isCollapsible(node, opts)) {
+      api.collapse(node);
+    } else {
+      api.expand(node, opts);
+    }
+  });
+
+  const sbgnmlText = file.loadFileText('samples/pc_signallingByBMP.sbgn.xml');
+  renderGraph(renderer, sbgnmlText);
+
 
   // glue events
   $('#graph-load').click(function () {
