8bc97678087e545b2cd8820622e6afb85537a061
d2fong <d2fong@uwaterloo.ca>
2017-04-07 19:10:27 -0400

    fixes broken render graph function (expand collapse works properly now)

--- src/renderGraph.js
+++ src/renderGraph.js
@@ -8,87 +8,15 @@ const removeDisconnectedNodes = (cy) => {
   danglingNodes.filterFn((ele) => !ele.isChild() && ele.neighborhood.length === 0).remove();
 };
 
-const expandCollapseComplexNodesBilkent = (cy) => {
-  const opts = {
-    fisheye: true,
-    animate: true,
-    undoable: false,
-    cueEnabled: false
-  };
-  cy.expandCollapse(opts);
-
-  cy.nodes().on('expandcollapse.afterexpand', function (evt) {
-    const node = evt.target;
-    cy.zoomingEnabled(false);
-    node.children().layout({
-      name:'grid',
-      fit: 'false',
-      avoidOverlap: true,
-      condense: true,
-      animate: true,
-      rows: node.children().size() / 2,
-      cols: node.children().size() / 2,
-      boundingBox: node.boundingBox()
-    }).run();
-    cy.zoomingEnabled(true);
-  });
-
-  const complexes = cy.nodes('[class="complex"], [class="complex multimer"]');
+const collapseComplexNodes = (cy) => {
   const api = cy.expandCollapse('get');
-  api.collapse(complexes);
-
-  cy.on('tap', 'node[class="complex"], node[class="complex multimer"]', (evt) => {
-    evt.preventDefault();
-    const node = evt.target;
-    if (api.isCollapsible(node, opts)) {
-      api.collapse(node);
-    } else {
-      api.expand(node, opts);
-    }
-  });
-};
-
-const expandCollapseComplexNodes = (cy) => {
-  const complexChildrenMap = new Map();
-
   const complexes = cy.nodes('[class="complex"], [class="complex multimer"]');
-  complexes.forEach((ele) => {
-    complexChildrenMap.set(ele.id(), {nodes: ele.descendants(), edges: ele.descendants().connectedEdges()});
-  });
-  complexes.descendants().remove();
-  cy.on('tap', 'node[class="complex"], node[class="complex multimer"]', (evt) => {
-
-    const node = evt.target;
-    const children = complexChildrenMap.get(node.id());
-
-    if (children) {
-      if (children.nodes.removed()) {
-        cy.zoomingEnabled(false);
-        children.nodes.layout({
-          name: 'grid',
-          condense: true,
-          avoidOverlap: true,
-          animate: true,
-          rows: 4,
-          cols: 4,
-          boundingBox: node.boundingBox()
-        }).run();
-        children.nodes.restore();
-        children.edges.restore();
-        cy.zoomingEnabled(true);
-      } else {
-        children.nodes.remove();
-        children.edges.remove();
-      }
-    }
-  });
+  api.collapseRecursively(complexes);
 };
-
 
 const reduceGraphComplexity = (cy) => {
   removeDisconnectedNodes(cy);
-  expandCollapseComplexNodes(cy);
-  // expandCollapseComplexNodesBilkent(cy);
+  collapseComplexNodes(cy);
 };
 
 const renderGraph = (cy, sbgnmlText) => {
