1eab28fbee71b67b4f8ce23e65b1711475604d11
d2fong <d2fong@uwaterloo.ca>
2017-04-17 11:25:53 -0400

    remove default option entries - they were already default in cy.js anyway - improves the scroll responsiveness by removing these options - default options are empty right now; can be added onto later

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ let sbgnStyleSheet = require('./sbgnStyle/graph');
 let renderGraph = require('./renderGraph');
 
 let defaultOptions = {
-  boxSelectionEnabled: true,
-  showOverlay: false,
-  minZoom: false,
-  maxZoom: false,
-  motionBlur: false,
-  wheelSensitivity: 0.1
 };
 
 class SBGNRenderer extends Cytoscape {
