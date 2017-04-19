e8940f95fb63de3eec091351972f1e8dba33c624
Dylan Fong <d2fong@uwaterloo.ca>
2017-04-19 13:14:33 -0400

    displays multiple cy instances for comparing layouts

--- demo/browserify-entry.js
+++ demo/browserify-entry.js
@@ -16,65 +16,65 @@ coseBilkent(SBGNRenderer.__proto__);
 const file = require('./app/file');
 const save = require('./app/save');
 
-const renderGraph = (renderer, sbgnmlText) => {
-  SBGNRenderer.renderGraph(renderer, sbgnmlText);
+const defaultText = file.loadFileText('samples/pc_signallingByBMP.sbgn.xml');
+const loadInEach = (renderers, sbgnmlText) => {
+  for (let r of renderers) {
+    SBGNRenderer.renderGraph(r, sbgnmlText);
+  }
 };
 
 $(document).ready(function () {
 
-  // init the renderer
-  var container = $('#sbgn-network-container');
-  var renderer = new SBGNRenderer({
-    container: container
-  });
-
-  window.SBGNRenderer = SBGNRenderer;
-  window.r = window.cy = renderer;
-
-  renderer.on('tap', 'node', function (evt) {
-    console.log(evt.target);
-  });
-  const opts = {
-    fisheye: true,
-    animate: true,
-    undoable: false,
-    cueEnabled: false
-  };
-  renderer.expandCollapse(opts);
-
-  renderer.on('expandcollapse.afterexpand', function (evt) {
-    const node = evt.target;
-    renderer.zoomingEnabled(false);
-    node.children().layout({
-      name:'grid',
-      fit: 'false',
-      avoidOverlap: true,
-      condense: true,
+  const containers = ['#default-layout', '#signalling-layout','#cose-bilkent-layout'];
+  const renderers = containers.map((container, index) => {
+    console.log(container, index);
+    const renderer = new SBGNRenderer({container: $(container)});
+    renderer.on('tap', 'node', function (evt) {
+      console.log(evt.target);
+    });
+    const opts = {
+      fisheye: true,
       animate: true,
-      rows: node.children().size() / 2,
-      cols: node.children().size() / 2,
-      boundingBox: node.boundingBox()
-    }).run();
-    renderer.zoomingEnabled(true);
+      undoable: false,
+      cueEnabled: false
+    };
+    renderer.expandCollapse(opts);
+
+    renderer.on('expandcollapse.afterexpand', function (evt) {
+      const node = evt.target;
+      renderer.zoomingEnabled(false);
+      node.children().layout({
+        name:'grid',
+        fit: 'false',
+        avoidOverlap: true,
+        condense: true,
+        animate: true,
+        rows: node.children().size() / 2,
+        cols: node.children().size() / 2,
+        boundingBox: node.boundingBox()
+      }).run();
+      renderer.zoomingEnabled(true);
+    });
+
+    const complexes = renderer.nodes('[class="complex"], [class="complex multimer"]');
+    const api = renderer.expandCollapse('get');
+    api.collapse(complexes);
+
+    renderer.on('tap', 'node[class="complex"], node[class="complex multimer"]', (evt) => {
+      evt.preventDefault();
+      const node = evt.target;
+      if (api.isCollapsible(node, opts)) {
+        api.collapse(node);
+      } else {
+        api.expand(node, opts);
+      }
+    });
+
+    window['cy' + index] = renderer;
+    return renderer;
   });
 
-  const complexes = renderer.nodes('[class="complex"], [class="complex multimer"]');
-  const api = renderer.expandCollapse('get');
-  api.collapse(complexes);
-
-  renderer.on('tap', 'node[class="complex"], node[class="complex multimer"]', (evt) => {
-    evt.preventDefault();
-    const node = evt.target;
-    if (api.isCollapsible(node, opts)) {
-      api.collapse(node);
-    } else {
-      api.expand(node, opts);
-    }
-  });
-
-  const sbgnmlText = file.loadFileText('samples/pc_signallingByBMP.sbgn.xml');
-  renderGraph(renderer, sbgnmlText);
-
+  loadInEach(renderers, defaultText);
 
   // glue events
   $('#graph-load').click(function () {
@@ -85,19 +85,20 @@ $(document).ready(function () {
     if ($(this).val() != '') {
       var f = this.files[0];
 
-      file.readFile(renderer, f, function (ft) {
-        renderGraph(renderer, ft.target.result);
+      file.readFile(f, function (ft) {
+        loadInEach(renderers, ft.target.result);
       });
     }
   });
 
+  // disabled for now
   $('#graph-save').click(function () {
-    save(renderer, 'graph');
+    save(renderers, 'graph');
   });
 
   $('.sample-file').click(function () {
     var fileText = file.loadFileText('samples/' + $(this)[0].innerText + '.xml');
-    renderGraph(renderer, fileText);
+    loadInEach(renderers, fileText);
   });
 
 });
